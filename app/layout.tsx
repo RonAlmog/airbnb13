136a1dadd59e56c7dff64647c21922c0692b0644
Ron Almog <ron8132@gmail.com>
2023-04-21 10:33:03 -0400

    modal and button components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import Modal from "@/components/modals/Modal";
 
 export const metadata = {
   title: "Airbnb",
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <Modal isOpen title="hello world" />
         <Navbar />
         {children}
       </body>
