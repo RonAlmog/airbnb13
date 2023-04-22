bb740cc7920c13047c9271ac1ef0d45456efd9e4
Ron Almog <ron8132@gmail.com>
2023-04-22 10:30:41 -0400

    add input control

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
 import Modal from "@/components/modals/Modal";
+import RegisterModal from "@/components/modals/RegisterModal";
 
 export const metadata = {
   title: "Airbnb",
@@ -18,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal isOpen title="hello world" />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
--- components/navbar/UserMenu.tsx
+++ components/navbar/UserMenu.tsx
@@ -3,9 +3,10 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
-type Props = {};
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
-const UserMenu = (props: Props) => {
+const UserMenu = () => {
+  const registerModal = useRegisterModal();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
@@ -15,14 +16,14 @@ const UserMenu = (props: Props) => {
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={toggleOpen}
+          onClick={() => {}}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           Airbnb your home
         </div>
         <div
-          onClick={() => {}}
-          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full hover:shadow-md transition"
+          onClick={toggleOpen}
+          className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
@@ -35,7 +36,7 @@ const UserMenu = (props: Props) => {
           <div className="flex flex-col cursor-pointer">
             <>
               <MenuItem onClick={() => {}} label="Login" />
-              <MenuItem onClick={() => {}} label="Signup" />
+              <MenuItem onClick={registerModal.onOpen} label="Signup" />
               <MenuItem onClick={() => {}} label="Login" />
             </>
           </div>
