f9cc731a2589aaaa0b917761698e714712163e70
Ron Almog <ron8132@gmail.com>
2023-05-02 18:26:02 -0400

    add loading page, and react-spinners

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import Loader from "@/components/Loader";
+import React from "react";
+
+type Props = {};
+
+const Loading = (props: Props) => {
+  return <Loader />;
+};
+
+export default Loading;
