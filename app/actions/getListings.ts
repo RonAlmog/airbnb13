12811d2dfc557f5e3896533de60f126b9ecb800e
Ron Almog <ron8132@gmail.com>
2023-05-02 13:14:28 -0400

    add search by url params

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -3,14 +3,66 @@ import { Listing } from "@prisma/client";
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
     let query: any = {};
+    // filter by owner (for owner's properties page)
     if (userId) {
       query.userId = userId;
+    }
+    // filter by category
+    if (category) {
+      query.category = category;
+    }
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount, // gte is greater or equal
+      };
+    }
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              { endDate: { gte: startDate }, startDate: { lte: startDate } },
+              { startDate: { lte: endDate }, endDate: { gte: endDate } },
+            ],
+          },
+        },
+      };
     }
     const listings = await prisma.listing.findMany({
       where: query,
